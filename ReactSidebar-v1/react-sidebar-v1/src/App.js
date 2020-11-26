98d1c4f5d95ecd264963868a41cfef68b14ce3b7
MBP <mosuliman@MBPs-MacBook-Pro.local>
2020-11-26 15:30:15 -0600

    v1 complete

--- ReactSidebar-v1/react-sidebar-v1/src/App.js
+++ ReactSidebar-v1/react-sidebar-v1/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <Router>
         <Navbar/>
         <Switch>
-          <Route path='/'/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/products' component={Products}/>
+          <Route path='/reports' component={Reports}/>
         </Switch>
 
       </Router>
--- ReactSidebar-v1/react-sidebar-v1/src/components/Navbar.js
+++ ReactSidebar-v1/react-sidebar-v1/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import {SidebarData } from './SidebarData';
+import './Navbar.css';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
+import { IconContext } from 'react-icons';
 
 
 function Navbar() {
@@ -12,6 +14,7 @@ function Navbar() {
 
     return (
         <>
+        <IconContext.Provider value={{color: '#fff'}}>
             <div className="navbar">
                 <Link to="#" className="menu-bars">
                     <FaIcons.FaBars onClick={showSidebar}/>
@@ -27,7 +30,7 @@ function Navbar() {
                     {SidebarData.map((item, index) => {
                         return(
                             <li key={index} className={item.cName}>
-                                <Link to={item.path}>
+                                <Link to={item.path} onClick={showSidebar}>
                                     {item.icon}
                                     <span>{item.title}</span>
                                 </Link>
@@ -37,6 +40,7 @@ function Navbar() {
 
                 </ul>
             </nav>
+            </IconContext.Provider>
         </>
     );    
 }
