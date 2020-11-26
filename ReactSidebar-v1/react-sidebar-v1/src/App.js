504bbebc23a9f6e0062380c68f29c2416d98bb0a
MBP <mosuliman@MBPs-MacBook-Pro.local>
2020-11-26 15:06:54 -0600

    updated App.js with new route switch

--- ReactSidebar-v1/react-sidebar-v1/src/App.js
+++ ReactSidebar-v1/react-sidebar-v1/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/'/>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
